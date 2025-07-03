b475f79c3808014176cf9407d9869ccee8f6f4d3
Yuvaraja T <tyuvaraja8@deloitte.com>
2025-07-03 18:14:16 +0530

    Updated script to reflect custom worker script

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function requestDownload(filename) {
     return;
   }
 
-  const res = await fetch("https://your-worker.workers.dev", {
+  const res = await fetch("https://dms-site-app-worker.tyuvaraja8.workers.dev/", {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ password, file: filename })
